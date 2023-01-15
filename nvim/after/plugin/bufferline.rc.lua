2df44403e1b7d930a61e3440e89503860d373c6b
Blackmage <blackmage@pop-os.localdomain>
2023-01-15 17:18:15 +0700

    feat: change bufferline separator style to thin

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -6,7 +6,7 @@ end
 bufferline.setup({
 	options = {
 		mode = "tabs",
-		separator_style = "slant",
+		separator_style = "thin",
 		always_show_bufferline = false,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
