d0cb2f16242c2546396116d3e8549867b7eda896
Blxckmage <fazadoang15@gmail.com>
2023-07-04 21:34:38 +0700

    fix: fix typo

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -4,9 +4,9 @@ require("core.keymaps")
 -- bootstrap lazy and run plugins
 require("core.lazy")
 
--- NOTE: catppuccin, rose-pine, tokyonight( can be found on lua/plugins/themes)
+-- NOTE: catppuccin, rose-pine, tokyonight(can be found on lua/plugins/themes)
 local name = "catppuccin"
-pcall(require, "plugins.theme." .. name)
+pcall(require, "plugins.themes." .. name)
 
 -- Set theme
 vim.cmd.colorscheme(name)
