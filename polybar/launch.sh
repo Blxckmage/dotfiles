6a9d288a2447bf7c3ee9b30f27fd45c01b3bb00b
Blxckmage <fazadoang@15@gmail.com>
2023-07-08 17:01:31 +0700

    feat: update picom and polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
+# polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
@@ -8,6 +10,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
