54cca78efed8f01fbbf1d463b47c3c3490e53dec
Blxckmage <fazadoang15@gmail.com>
2023-09-30 22:41:26 +0700

    feat: add oil.nvim

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -66,4 +66,33 @@ return {
 			require("treesitter-context").setup({})
 		end,
 	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		config = function()
+			require("oil").setup({
+				columns = {
+					"icon",
+					"size",
+				},
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["<C-s>"] = "actions.select_vsplit",
+					["<C-h>"] = "actions.select_split",
+					["<C-t>"] = "actions.select_tab",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = "actions.close",
+					["<C-l>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = "actions.tcd",
+					["g."] = "actions.toggle_hidden",
+				},
+			})
+
+			vim.keymap.set("n", "<Leader>=", require("oil").open, { desc = "Open parent directory" })
+		end,
+	},
 }
