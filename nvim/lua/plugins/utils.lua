8df58b707b59a338ae1a7680099ba0079c1f2f81
Blxckmage <fazadoang15@gmail.com>
2023-12-23 23:38:44 +0700

    fix: i have 2 nvim-treesitter-context

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -26,13 +26,6 @@ return {
 		end,
 	},
 	{
-		"nvim-treesitter/nvim-treesitter-context",
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			require("treesitter-context").setup({})
-		end,
-	},
-	{
 		"iamcco/markdown-preview.nvim",
 		build = function()
 			vim.fn["mkdp#util#install"]()
