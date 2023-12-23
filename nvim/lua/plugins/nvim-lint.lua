0c722742d08f89a0b8667b9f645228e0ac9961b3
Blxckmage <fazadoang15@gmail.com>
2023-12-23 23:38:28 +0700

    fix: change to eslint cuz i have eslint and eslint_d at the same time lol

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -3,11 +3,11 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("lint").linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
+			javascript = { "eslint" },
+			typescript = { "eslint" },
+			javascriptreact = { "eslint" },
+			typescriptreact = { "eslint" },
+			svelte = { "eslint" },
 			python = { "pylint" },
 		}
 
