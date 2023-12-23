979a4dd8e9f882312696faa1e9eb587c175e4f17
Blxckmage <fazadoang15@gmail.com>
2023-12-23 23:39:33 +0700

    refactor: easier to see

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,51 +1,62 @@
-local opt = vim.opt
-local g = vim.g
+local options = {
+	clipboard = "unnamed,unnamedplus", --- Copy-paste between vim and everything else
+	cmdheight = 0, --- Give more space for displaying messages
+	completeopt = "menu,menuone,noselect", --- Better autocompletion
+	emoji = false, --- Fix emoji display
+	expandtab = true, --- Use spaces instead of tabs
+	ignorecase = true, --- Needed for smartcase
+	laststatus = 3, --- Have a global statusline at the bottom instead of one for each window
+	mouse = "a", --- Enable mouse
+	number = true, --- Shows current line number
+	relativenumber = true, --- Enable relativenumber
+	pumblend = 10, --- Enable transparancy for completion menu
+	pumheight = 10, --- Max num of items in completion menu
+	scrolloff = 8, --- Always keep space when scrolling to bottom/top edge
+	shiftwidth = 2, --- Change a number of space characters inserted for indentation
+	showtabline = 2, --- Always show tabs
+	smartcase = true, --- Uses case in search
+	smartindent = true, --- Makes indenting s/mart
+	smarttab = true, --- Makes tabbing smarter
+	splitbelow = true, --- Horizontal splits will automatically be to the bottom
+	splitright = true, --- Vertical splits will automatically be to the right
+	swapfile = false, --- Useless fuck
+	tabstop = 2, --- Insert 2 spaces for a tab
+	softtabstop = 2, --- Insert 2 spaces for a tab
+	termguicolors = true, --- Correct terminal colors
+	timeoutlen = 200, --- Faster completion
+	updatetime = 100, --- Faster completion
+	wildignore = { "*/node_modules/**" }, --- Don't search inside Node.js modules
+	wrap = false, --- Turn off wrap
+	backup = false, --- Not needed
+	writebackup = false, --- Not needed
+	autoindent = true, --- Good auto indent
+	breakindent = true, --- Break indent
+	backspace = "indent,eol,start", --- Making sure backspace works
+	showmode = true, --- Shows mode
+	undofile = true, --- Sets undo to file
+	background = "dark", --- Sets default background to dark
+	incsearch = true, --- Start searching before pressing enter
+	encoding = "utf-8", --- The encoding displayed
+	fileencoding = "utf-8", --- The encoding written to file
+	autowrite = true, --- Auto-save before certain operations
+	showmatch = true, --- Highlight matching brackets
+	hlsearch = false, --- Disable search result highlighting
+	title = true, --- Set window title to current file name
+	backupskip = "/tmp/*,/private/tmp*", --- Skip backup for specific directories
+	grepformat = "%f:%l:%c:%m", --- Format for parsing search results (File:Line:Column:Message)
+	grepprg = "rg --vimgrep --no-heading --smart-case", --- Set the external grep program (ripgrep) with Vim grep options
+}
 
-g.mapleader = " "
+local globals = {
+	mapleader = " ", --- Map leader key to SPC
+}
 
-opt.encoding = "utf-8"
-opt.fileencoding = "utf-8"
-opt.number = true
-opt.relativenumber = true
-opt.autowrite = true
-opt.clipboard = "unnamedplus"
-opt.completeopt = "menu,menuone,noselect"
-opt.incsearch = true
-opt.showmatch = true
-opt.hlsearch = false
-opt.ignorecase = true
-opt.title = true
-opt.cmdheight = 1
-opt.laststatus = 2
-opt.showtabline = 2
-opt.backup = false
-opt.swapfile = false
-opt.backupskip = "/tmp/*,/private/tmp*"
+vim.opt.path:append({ "**" })
 
-if vim.fn.executable("rg") == 1 then
-	opt.grepformat = "%f:%l:%c:%m"
-	opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+for k, v in pairs(options) do
+	vim.opt[k] = v
 end
 
-opt.autoindent = true
-opt.expandtab = true
-opt.smarttab = true
-opt.smartindent = true
-opt.breakindent = true
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.wrap = false
-opt.mouse = "a"
-opt.pumblend = 10
-opt.pumheight = 10
-opt.scrolloff = 8
-opt.showcmd = true
-opt.showmode = true
-opt.spelllang = { "en" }
-opt.splitbelow = true
-opt.splitright = true
-opt.termguicolors = true
-opt.undofile = true
-opt.background = "dark"
-opt.path:append({ "**" })
-opt.wildignore:append({ "*/node_modules/*" })
+for k, v in pairs(globals) do
+	vim.g[k] = v
+end
