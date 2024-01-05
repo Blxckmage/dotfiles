c442eb0237da7c4413ac49a17873ab0f6fef8a5d
Blxckmage <fazadoang15@gmail.com>
2024-01-05 18:50:05 +0700

    feat: add mini.animate and mini.pairs

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -45,4 +45,43 @@ return {
 			},
 		},
 	},
+	{
+		"echasnovski/mini.animate",
+		event = "VeryLazy",
+		opts = function()
+			local mouse_scrolled = false
+			for _, scroll in ipairs({ "Up", "Down" }) do
+				local key = "<ScrollWheel" .. scroll .. ">"
+				vim.keymap.set({ "", "i" }, key, function()
+					mouse_scrolled = true
+					return key
+				end, { expr = true })
+			end
+
+			local animate = require("mini.animate")
+			return {
+				resize = {
+					timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+				},
+				scroll = {
+					timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+					subscroll = animate.gen_subscroll.equal({
+						predicate = function(total_scroll)
+							if mouse_scrolled then
+								mouse_scrolled = false
+								return false
+							end
+							return total_scroll > 1
+						end,
+					}),
+				},
+				open = {
+					enable = false,
+				},
+				close = {
+					enable = false,
+				},
+			}
+		end,
+	},
 }
