6784e3a534eaa663c6ba0c217390997bccdfa6a0
Blxckmage <fazadoang15@gmail.com>
2024-01-05 18:50:10 +0700

    feat: add harpoon

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,27 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup({})
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():append()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end)
+	end,
+}
