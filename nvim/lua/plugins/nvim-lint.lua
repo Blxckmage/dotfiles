27281c51b7d9b45d2d894ab83550f183e8aab135
Blxckmage <fazadoang15@gmail.com>
2024-01-12 10:45:42 +0700

    fix: replace eslint with eslint_d

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -3,12 +3,12 @@ return {
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	config = function()
 		require("lint").linters_by_ft = {
-			javascript = { "eslint" },
-			typescript = { "eslint" },
-			javascriptreact = { "eslint" },
-			typescriptreact = { "eslint" },
-			svelte = { "eslint" },
-			python = { "pylint" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+			python = { "flake8" },
 		}
 
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
