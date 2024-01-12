e7eb472370a1ee85cc8b12a88cd87a50ff5defa8
Blxckmage <fazadoang15@gmail.com>
2024-01-12 10:46:07 +0700

    feat: add copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+	"zbirenbaum/copilot.lua",
+	opts = {
+		suggestion = {
+			accept = false,
+			enabled = true,
+			auto_trigger = true,
+		},
+		panel = {
+			enabled = true,
+			auto_refresh = false,
+		},
+	},
+}
