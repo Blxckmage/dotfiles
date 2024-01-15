6b26d13ceaed95a5b8060bc7f8652af200f6ccd0
Blxckmage <fazadoang15@gmail.com>
2024-01-15 22:08:03 +0700

    fix: turn off notifier for checker and turn on change_detection

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -20,7 +20,11 @@ require("lazy").setup({
 		lazy = false,
 		version = false,
 	},
-	checker = { enabled = true },
+	checker = { enabled = true, notify = false },
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
 	performance = {
 		rtp = {
 			-- disable some rtp plugins
