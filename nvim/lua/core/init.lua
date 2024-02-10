92d8752a96366ce14219d4ee52d0dd8a382595a8
Blxckmage <fazadoang15@gmail.com>
2024-02-10 13:56:57 +0700

    refactor: update init.lua

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -5,9 +5,5 @@ require("core.autocmds")
 -- bootstrap lazy and run plugins
 require("core.lazy")
 
--- NOTE: catppuccin, rose-pine, tokyonight(can be found on lua/plugins/themes)
-local name = "catppuccin"
-pcall(require, "plugins.themes." .. name)
-
 -- Set theme
-vim.cmd.colorscheme(name)
+vim.cmd.colorscheme("catppuccin")
