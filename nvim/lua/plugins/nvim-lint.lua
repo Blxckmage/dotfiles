8f47c502d186ee92f78bef0779a577be0ed81edb
Blxckmage <fazadoang15@gmail.com>
2024-02-11 21:13:41 +0700

    fix: change javascript and typescript linter to use eslint cuz eslint_d searches for configuration files and its a pain in the ass to make config file everytime i wanted to run js/ts

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -3,8 +3,8 @@ return {
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	config = function()
 		require("lint").linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
+			javascript = { "eslint" },
+			typescript = { "eslint" },
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
