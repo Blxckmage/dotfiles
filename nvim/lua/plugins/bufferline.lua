34c982a2f1b36e43b4e3482c2b63dd6a3cd23c0a
Blxckmage <fazadoang15@gmail.com>
2024-02-11 21:11:45 +0700

    perf(bufferline): update bufferline config

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,22 +1,22 @@
 return {
 	"akinsho/bufferline.nvim",
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		local bufferline = require("bufferline")
-		local keymap = vim.keymap
-
-		bufferline.setup({
-			options = {
-				mode = "tabs",
-				separator_style = "thin",
-				always_show_bufferline = false,
-				show_buffer_close_icons = false,
-				show_close_icon = false,
-				color_icons = true,
-			},
-		})
-
-		keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", {})
-		keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", {})
+	event = "VeryLazy",
+	lazy = true,
+	keys = {
+		{ "<Tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Next bufffer" },
+		{ "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
+	},
+	opts = {
+		options = {
+			mode = "tabs",
+			separator_style = "thin",
+			always_show_bufferline = false,
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+			color_icons = true,
+		},
+	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
 	end,
 }
